c746b10c0d7206175a87913571b405fc2e053067
leopoldkwok <leopoldkwok@gmail.com>
2014-11-20 16:49:31 +0000

    added 3 post test and upload images

---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ActiveRecord::Base
+  has_attached_file :picture,
+	  styles: { medium: "300x300>" },
+		 :storage => :s3,
+   		:s3_credentials => {
+     		:bucket => 'instagramclonev2',
+     		:access_key_id => Rails.application.secrets.s3_access_key_id,
+     		:secret_access_key => Rails.application.secrets.s3_secret_access_key
+
+		}
+
+	validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+end
